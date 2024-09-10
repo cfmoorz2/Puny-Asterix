7cd02aeab7a77d2e0537f0891e45ffe767dc4393
Charles Moore <chasfmoorejr@gmail.com>
2024-09-10 15:57:08 -0400

    added some basement rooms

--- rooms.h
+++ rooms.h
@@ -1,4 +1,4 @@
-Object morgue "Morgue"
+Room morgue "Morgue"
     with name 'morgue',
         description [;
                 print "This is the hospital's small morgue. The white tiled floor is scuffed and gently slopes toward a drain in the center of the
@@ -7,5 +7,144 @@ Object morgue "Morgue"
                 lockers. A door lies to the west.^^
                 A body is lying on the steel table.^";
             ],
-        w_to 0,
+        w_to morgue_door,
     has light;
+
+ !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ myDoor morgue_door "morgue door" 
+    with name 'door',
+        description [;
+            print"It's an unremarkable door, currently ";
+            open_or_closed(self);
+            ".";
+        ],
+        door_to [;
+            if (parent(self) == morgue) return basement_hallway_east; return morgue;
+        ],
+        door_dir [;
+            if (parent(self) == morgue) return w_to; return e_to;
+        ],
+        found_in morgue basement_hallway_east,
+    has scenery door openable ~open;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room basement_hallway_east "Basement Hallway"
+    with description "This is the eastern end of a long sterile hallway that continues far to the west. Linoleum tiles cover the floor. A door
+                    leads east, a placard next to it reads ~Morgue~.",
+        e_to morgue_door,
+        w_to elevator_lobby_b,
+    has light; 
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room elevator_lobby_b "elevator_lobby_b" 
+    with description [; 
+        print"This is a spartan fluorescent-lit hallway that continues to the east and west.  A stairwell lies to the north through an 
+        open doorway. Opposite the stairwall in the south wall stands the main elevator. Next to it you see a sign reading ~Morgue~ above an arrow 
+        pointing east and one reading ~Central Supply~ above one pointing west. The elevator doors are currently ";
+        !open_or_closed(elevator_doors);
+        ". The call button is embedded in a small panel next to the door.";
+        ],
+        e_to basement_hallway_east,
+        n_to stairwell_b,
+        !s_to elevator_doors,
+        !in_to elevator_doors,
+        w_to basement_hallway_west,
+    has light,
+    ;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room stairwell_b "stairwell_b" 
+    with description
+        "This is a dark stairwell just off the main hallway which lies to the south. A large ~B~ is painted on the wall. 
+        Concrete stairs zig-zag upward to the floor above.",
+        s_to elevator_lobby_b,
+        u_to 0,
+    has light stairs,
+    ;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room basement_hallway_west "basement_hallway_west"
+    with description
+        "This is nearly the midpoint of a long dank east-west hallway. A service elevator is here to the north. A sign next to it 
+        reads ~Employees Only~.",
+        e_to elevator_lobby_b,
+        w_to hallway_b1,
+        !n_to service_elevator_door,
+        !in_to service_elevator_door,
+        has light,
+        ;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room hallway_b1 "hallway_b1"
+    with description "The corridor here continues east and west. Embedded here in the wall to the south you see an 
+    old dumbwaiter, presumably once used to deliver needed items to the patient floors above.",
+    e_to basement_hallway_west,
+    w_to hallway_b2,
+    has light;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room hallway_b2 "hallway_b2"
+    with description [;
+        print"This is the western end of a long corridor that stretches to the east under flickering fluorescent lights. 
+        There's an open doorway to the north, a sign next to it reads ~Environmental Services~.
+        A wooden door lies to the west. It's currently ";
+        (open_or_closed(engineering_door)); 
+        print"and a sign next to it reads ~Engineering~. Finally, a substantial metal door lies to the south, a placard on it reads 
+        ~Central Supply.~ There's a magnetic key card swipe next to it, a small light on it is^";
+        !if (card_reader has on) "green."; else "red.";
+        ],
+        after [;
+            Go:
+                if (noun == n_obj) {
+                    !give central_supply_door ~open;
+                    !give central_supply_door locked;
+                    !give card_reader ~on;
+                    "As you pass through the door, it closes behind you with a 'click'. The light on the card reader turns red. ";
+                }
+        ],
+        e_to hallway_b1,
+        n_to environmental_services,
+        !s_to central_supply_door,
+        w_to engineering_door,
+    has light;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room environmental_services "Environmental Services"
+    with description [;
+        print"This small cement-lined space is filled with various containers of cleaning solvents, brushes, "; 
+        !if (mop_bucket in self) print "a mop bucket, ";
+        print"and brooms. ";
+        print"A small wooden desk is shoved into one corner. ";
+        "The exit is to the south. ";
+    ],
+    s_to hallway_b2,
+    has light;
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Room engineering "Engineering"
+    with description "This is a small cramped room that smells vaguely of ozone. A metal workbench 
+        takes up most of the available space and electrical conduits descend like a massive pipe organ from 
+        the ceiling into a large fuse box on the wall. The only exit is east through a plain metal door. ",
+        before [;
+            Smell:
+                "It smells mechanical in here, with a hint of electricity. ";
+        ],
+    e_to engineering_door,
+    has light;
+
+ !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ myDoor engineering_door "door to engineering" 
+    with name 'door' 'engineering' 'plain' 'metal',
+        description [;
+            print"It's a plain metal door, currently ";
+            open_or_closed(self);
+            print ".";
+        ],
+        door_to [;
+            if (parent(self) == hallway_b2) return engineering; return hallway_b2;
+        ],
+        door_dir [;
+            if (parent(self) == hallway_b2) return w_to; return e_to;
+        ],
+        found_in engineering hallway_b2,
+    has scenery door openable ~open;
