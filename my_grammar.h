f41e78fa9dce259f52ec87ba22eb3696a20a9055
cfmoorz2 <133705826+cfmoorz2@users.noreply.github.com>
2024-09-10 20:07:44 -0400

    following

--- my_grammar.h
+++ my_grammar.h
@@ -46,3 +46,64 @@ Extend 'turn'
 
 [FlipSub ; "Flipping over ",(the)noun," seems unproductive. "; ];
 
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!   FOLLOW
+[FollowScope direction one_out;
+    if(scope_stage == 1) rfalse;
+    if(scope_stage == 3) "There doesn't seem to be follow-able. ";
+    objectloop (direction in compass)
+    {
+        one_out = real_location.(direction.door_dir);
+        !print"one_out start = ",(name)one_out,"^";
+        if (one_out ofclass myDoor)
+        {
+            one_out = one_out.door_to(direction.door_dir);
+            !print"one_out DOOR = ",(name)one_out,"^";
+        } 
+        if (one_out ofclass Room)
+        {
+            ScopeWithin(one_out);
+        }
+    } 
+    rfalse;     
+];
+
+Verb 'follow'
+    * scope=FollowScope -> Follow;
+
+[FollowSub direction one_out; 
+    !print"here noun = ",(name)noun,"^";
+    if (noun == player) "You're possessing ",(name)noun," right now.";
+    if(noun in real_location) { print(The)noun," is already here.^"; rtrue; }
+    objectloop (direction in compass)
+    {
+        one_out = real_location.(direction.door_dir);
+        !print"one_out start = ",(name)one_out,"^";
+        if (one_out ofclass myDoor)
+        {
+            one_out = one_out.door_to(direction.door_dir);
+            !print"one_out DOOR = ",(name)one_out,"^";
+        } 
+        if (one_out ofclass Room)
+        {
+            if (noun in one_out) 
+            {
+                print"You follow ",(the)noun,;
+                if (direction == u_obj) { print" upstairs.^"; <<go direction>>;  }
+                if (direction == d_obj) { print" downstairs.^"; <<go direction>>; }
+                print" to the ",(name)direction,".^"; 
+                following = true;
+
+                switch(direction) 
+                {
+                    n_obj: <<go FAKE_N_OBJ>>;
+                    s_obj: <<go FAKE_S_OBJ>>;
+                    e_obj: <<go FAKE_E_OBJ>>;
+                    w_obj: <<go FAKE_W_OBJ>>;
+                    u_obj: <<go FAKE_U_OBJ>>;
+                    d_obj: <<go FAKE_D_OBJ>>;
+                }
+            }
+        }
+    } 
+];
